88a9148b7489ec4c99fddf91638c3049ce1b00e5
Eren Malkoç <105520516+erenmalkoc@users.noreply.github.com>
2024-12-04 00:27:27 +0300

    Optimize performance, added status bar, and fix bugs.
    - Added a status bar for better user feedback during video conversion.
    - Improved overall performance for faster video processing.
    - Updated functions for more accurate progress calculation.

--- app/src/main/java/dev/erenmalkoc/asyncvideoconverter/ui/ConverterViewModel.kt
+++ app/src/main/java/dev/erenmalkoc/asyncvideoconverter/ui/ConverterViewModel.kt
@@ -1,9 +1,9 @@
 package dev.erenmalkoc.asyncvideoconverter.ui
 
-
 import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Context
+import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
@@ -13,6 +13,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.arthenica.ffmpegkit.FFmpegKit
+import com.arthenica.ffmpegkit.FFmpegSession
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -22,9 +23,14 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 
 class ConverterViewModel : ViewModel() {
+
+    private val _progressState = MutableStateFlow<List<Float>>(emptyList())
+    val progressState: StateFlow<List<Float>> = _progressState.asStateFlow()
 
     private val _uiState = MutableStateFlow(ConverterUiState())
     val uiState: StateFlow<ConverterUiState> = _uiState.asStateFlow()
@@ -33,93 +39,198 @@ class ConverterViewModel : ViewModel() {
     val convertedFiles: LiveData<List<File>> get() = _convertedFiles
 
     fun setFilePaths(filePaths: List<String>) {
-        _uiState.value = uiState.value.copy(filePaths = filePaths)
+        _uiState.value = _uiState.value.copy(filePaths = filePaths)
     }
 
     fun clearFilePaths() {
-        _uiState.value = _uiState.value.copy(filePaths = listOf(), conversionInProgress = false, conversionSuccess = null)
+        _uiState.value = _uiState.value.copy(
+            filePaths = listOf(),
+            conversionInProgress = false,
+            conversionSuccess = null
+        )
     }
 
-    suspend fun convertVideosToMp3Parallel(
+    fun initializeProgressState(size: Int) {
+        _progressState.value = List(size) { 0f }
+    }
+
+
+    private fun updateProgress(index: Int, progress: Float) {
+
+        val currentProgress = _progressState.value.toMutableList()
+        if (index in currentProgress.indices) {
+            currentProgress[index] = progress
+            _progressState.value = currentProgress
+        }
+    }
+
+
+    fun startConversion(
+        context: Context,
         uris: List<Uri>,
-        contentResolver: ContentResolver,
-        outputDir: File
-    ): List<File?> {
-        return withContext(Dispatchers.IO) {
-            uris.map { uri ->
-                async {
-                    convertSingleVideoToMp3(uri, contentResolver, outputDir)
-                }
-            }.awaitAll()
+        outputDir: File,
+        contentResolver: ContentResolver
+    ) {
+        viewModelScope.launch {
+            _uiState.value =
+                _uiState.value.copy(conversionInProgress = true, conversionSuccess = null)
+
+            initializeProgressState(uris.size)
+
+            val outputFiles = withContext(Dispatchers.IO) {
+                uris.mapIndexed { index, uri ->
+                    async {
+                        convertSingleVideoToMp3(uri, contentResolver, outputDir, index)
+
+                    }
+                }.awaitAll()
+            }
+
+
+            val successfulFiles = outputFiles.filterNotNull()
+            if (successfulFiles.size == uris.size) {
+                successfulFiles.forEach { addFileToMediaLibrary(context, it) }
+                _uiState.value =
+                    _uiState.value.copy(conversionInProgress = false, conversionSuccess = true)
+                _convertedFiles.postValue(successfulFiles)
+            } else {
+                Log.e("ConverterViewModel", "Some files failed to convert.")
+                _uiState.value =
+                    _uiState.value.copy(conversionInProgress = false, conversionSuccess = false)
+            }
         }
     }
 
     private suspend fun convertSingleVideoToMp3(
         uri: Uri,
         contentResolver: ContentResolver,
-        outputDir: File
+        outputDir: File,
+        index: Int
     ): File? {
+        val tempInputFile = File(outputDir, "temp_input_${System.currentTimeMillis()}.mp4")
         val outputFile = File(outputDir, "converted_audio_${System.currentTimeMillis()}.mp3")
-        val inputFilePath = contentResolver.openInputStream(uri)?.use { inputStream ->
-            val tempInputFile = File(outputDir, "temp_input_${System.currentTimeMillis()}.mp4")
-            tempInputFile.outputStream().use { outputStream ->
-                inputStream.copyTo(outputStream)
-            }
-            tempInputFile.absolutePath
-        } ?: return null
 
-        val commandString = "-i $inputFilePath -vn -ar 44100 -ac 2 -b:a 192k ${outputFile.absolutePath}"
-        val ffmpegResult = FFmpegKit.execute(commandString)
+        return withContext(Dispatchers.IO) {
+            try {
+                contentResolver.openInputStream(uri)?.use { inputStream ->
+                    tempInputFile.outputStream().use { outputStream ->
+                        inputStream.copyTo(outputStream)
+                    }
+                } ?: throw Exception("Failed to copy input stream.")
 
-        return if (ffmpegResult.returnCode.isValueSuccess) {
-            outputFile
-        } else {
-            null
-        }
-    }
+                val totalDurationMs = getVideoDurationUsingTempFile(contentResolver, uri)
 
-    fun startConversion(context: Context, uris: List<Uri>, outputDir: File, contentResolver: ContentResolver) {
-        viewModelScope.launch {
+                val commandString =
+                    "-i ${tempInputFile.absolutePath} -vn -ar 44100 -ac 2 -b:a 192k ${outputFile.absolutePath}"
+                Log.d("FFmpegCommand", "Command: $commandString")
 
-            _uiState.value = _uiState.value.copy(conversionInProgress = true, conversionSuccess = null)
-
-            val outputFiles = convertVideosToMp3Parallel(
-                uris = uris,
-                contentResolver = contentResolver,
-                outputDir = outputDir
-            )
-
-
-            if (outputFiles.isNotEmpty() && outputFiles.all { it != null }) {
-                outputFiles.filterNotNull().forEach { file ->
-                    addFileToMediaLibrary(context, file)
-                    Log.d("BrowseFilesScreen", "File saved: ${file.absolutePath}")
+                val isSuccess = executeFFmpegCommandAsync(commandString, index, totalDurationMs)
+                if (isSuccess) {
+                    Log.d("ConverterViewModel", "FFmpeg command completed successfully.")
+                } else {
+                    Log.e("ConverterViewModel", "FFmpeg command failed.")
                 }
-                _uiState.value = _uiState.value.copy(conversionInProgress = false, conversionSuccess = true)
-            } else {
-                Log.e("BrowseFilesScreen", "Conversion failed for some files.")
-                _uiState.value = _uiState.value.copy(conversionInProgress = false, conversionSuccess = false)
+
+                if (outputFile.exists()) {
+                    outputFile
+                } else {
+                    Log.e("ConverterViewModel", "Output file not created: ${outputFile.absolutePath}")
+                    null
+                }
+            } catch (e: Exception) {
+                Log.e("ConverterViewModel", "Error during conversion: ${e.message}")
+                null
+            } finally {
+                if (tempInputFile.exists()) {
+                    tempInputFile.delete()
+                }
             }
         }
     }
 
 
+    suspend fun getVideoDurationUsingTempFile(contentResolver: ContentResolver, uri: Uri): Long {
+        val retriever = MediaMetadataRetriever()
+        val tempFile = File.createTempFile("temp_video", ".mp4")
 
-    fun addFileToMediaLibrary(context: Context, file: File) {
+        return withContext(Dispatchers.IO) {
+            try {
+
+                contentResolver.openInputStream(uri)?.use { inputStream ->
+                    tempFile.outputStream().use { outputStream ->
+                        inputStream.copyTo(outputStream)
+                    }
+                } ?: throw Exception("Failed to copy input stream to temporary file.")
+
+
+                retriever.setDataSource(tempFile.absolutePath)
+                val durationStr = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+                durationStr?.toLongOrNull() ?: 0L
+            } catch (e: Exception) {
+                Log.e("getVideoDurationUsingTempFile", "Error getting video duration: ${e.message}")
+                0L
+            } finally {
+                retriever.release()
+                tempFile.delete()
+            }
+        }
+    }
+
+
+    suspend fun executeFFmpegCommandAsync(
+        command: String,
+        index: Int,
+        totalDurationMs: Long
+    ): Boolean {
+        return suspendCoroutine { continuation ->
+            FFmpegKit.executeAsync(command,
+                { session ->
+                    val returnCode = session.returnCode
+                    val logs = session.allLogsAsString
+                    Log.d("FFmpegKitSession", "Session completed with return code: $returnCode")
+                    Log.d("FFmpegLogs", logs)
+
+                    if (returnCode.isValueSuccess) {
+                        viewModelScope.launch(Dispatchers.Main) {
+                            updateProgress(index, 100f)
+                        }
+                        continuation.resume(true)
+                    } else {
+                        continuation.resume(false)
+                    }
+                },
+                { log ->
+                    Log.d("FFmpegKitLog", log.message)
+                },
+                { statistics ->
+                    val currentTimeMs = statistics.time
+                    if (totalDurationMs > 0) {
+                        val progress = (currentTimeMs.toFloat() / totalDurationMs.toFloat()) * 100
+
+
+                        viewModelScope.launch(Dispatchers.Main) {
+                            updateProgress(index, progress)
+                        }
+
+                        Log.d("ConverterViewModel", "İlerleme durumu: $progress%")
+                    }
+                }
+            )
+        }
+    }
+
+    private fun addFileToMediaLibrary(context: Context, file: File) {
         val values = ContentValues().apply {
             put(MediaStore.MediaColumns.DISPLAY_NAME, file.name)
             put(MediaStore.MediaColumns.MIME_TYPE, "audio/mpeg")
             put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_MUSIC)
         }
 
-        val uri =
-            context.contentResolver.insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values)
-
-        uri?.let {
-            context.contentResolver.openOutputStream(it).use { outputStream ->
-                file.inputStream().copyTo(outputStream!!)
-            }
-        }
+        context.contentResolver.insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values)
+            ?.let { uri ->
+                context.contentResolver.openOutputStream(uri)?.use { outputStream ->
+                    file.inputStream().copyTo(outputStream)
+                }
+            } ?: Log.e("ConverterViewModel", "Failed to add file to media library.")
     }
-
 }
--- app/src/main/java/dev/erenmalkoc/asyncvideoconverter/ui/screen/BrowseFilesScreen.kt
+++ app/src/main/java/dev/erenmalkoc/asyncvideoconverter/ui/screen/BrowseFilesScreen.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -15,8 +16,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.erenmalkoc.asyncvideoconverter.R
@@ -42,7 +45,12 @@ fun BrowseFilesScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Image(
+            painter = painterResource(id = R.drawable.ic_autorenew),
+            contentDescription = "Conversion Icon",
+            modifier = Modifier.size(80.dp).padding(mediumPadding),
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary)
+        )
 
         Text(
             text = "Select the files you want to convert.",
@@ -57,58 +65,44 @@ fun BrowseFilesScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-
             VideoPicker(onVideosPicked = { uris ->
                 converterViewModel.setFilePaths(uris.map { it.toString() })
                 uris.forEach { uri -> onBrowseClicked(uri) }
             })
-
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(mediumPadding),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Card(
-                    modifier = Modifier.padding(mediumPadding),
-                    elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+            if (converterUiState.filePaths.isNotEmpty()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(mediumPadding),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Column(
+                    Card(
                         modifier = Modifier.padding(mediumPadding),
-                        verticalArrangement = Arrangement.spacedBy(mediumPadding),
-                        horizontalAlignment = Alignment.Start
+                        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
                     ) {
+                        Column(
+                            modifier = Modifier.padding(mediumPadding),
+                            verticalArrangement = Arrangement.spacedBy(mediumPadding),
+                            horizontalAlignment = Alignment.Start
+                        ) {
 
-                        Text(
-                            text = "Selected Files: ",
-                            style = MaterialTheme.typography.titleMedium ,
-                        )
-
-                        converterUiState.filePaths.forEachIndexed { index, filePath ->
                             Text(
-                                text = filePath,
-                                style = MaterialTheme.typography.bodyMedium,
+                                text = "Selected Files: ",
+                                style = MaterialTheme.typography.titleMedium,
                             )
 
-                            if (index < converterUiState.filePaths.size - 1) {
-                                HorizontalDivider(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(vertical = 4.dp)
-                                )
-                            }
-                        }
+                            VideoConversionProgress(
+                                converterViewModel = converterViewModel,
+                                videoPaths = converterUiState.filePaths
+                            )
 
-
-                        if (converterUiState.filePaths.isNotEmpty()) {
                             val context = LocalContext.current
                             Row(
                                 modifier = Modifier.fillMaxWidth(),
                                 horizontalArrangement = Arrangement.spacedBy(8.dp),
                                 verticalAlignment = Alignment.CenterVertically
                             ) {
-
 
                                 OutlinedButton(
                                     onClick = { converterViewModel.clearFilePaths() },
@@ -117,7 +111,6 @@ fun BrowseFilesScreen(
                                 }
 
                                 Spacer(modifier = Modifier.weight(1f))
-
 
                                 Button(
                                     onClick = {
@@ -140,15 +133,15 @@ fun BrowseFilesScreen(
                                 }
                             }
                         }
-
-
-                        ConversionStatus(converterUiState = converterUiState)
                     }
                 }
             }
+
+            ConversionStatus(converterUiState = converterUiState)
         }
     }
 }
+
 
 
 @Composable
@@ -173,24 +166,77 @@ fun VideoPicker(onVideosPicked: (List<Uri>) -> Unit) {
 }
 
 @Composable
+fun VideoConversionProgress(
+    converterViewModel: ConverterViewModel,
+    videoPaths: List<String>
+) {
+    val progressState by converterViewModel.progressState.collectAsState()
+
+    Column {
+        videoPaths.forEachIndexed { index, path ->
+            val progress = progressState.getOrNull(index) ?: 0f
+            val progressPercentage = (progress).toInt()
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Video ${index + 1}: $path",
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.weight(1f)
+                )
+
+                if (progressPercentage > 0) {
+                    Text(
+                        text = "$progressPercentage%",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(start = 8.dp),
+                        color = Color.Red
+                    )
+                } else {
+                    Text(
+                        text = "Waiting...",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(start = 8.dp),
+                        color = Color.Gray
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
 fun ConversionStatus(converterUiState: ConverterUiState) {
     if (converterUiState.conversionInProgress) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            CircularProgressIndicator(modifier = Modifier.padding(top = 16.dp),)
-            Spacer(modifier = Modifier.size(10.dp))
-            Text(text = "Converting... Please Wait", style = MaterialTheme.typography.titleSmall)
-
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .padding(end = 10.dp)
+                    .align(Alignment.CenterVertically)
+            )
+            Text(
+                text = "Converting... Please Wait",
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
         }
     } else {
         when (converterUiState.conversionSuccess) {
             true -> Text(
                 text = "Conversion successful! Check your files.",
                 style = MaterialTheme.typography.titleMedium,
-                color = Color.Green
+                color = Color.Blue
             )
 
             false -> Text(
